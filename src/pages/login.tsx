04da80b8bad662e8d2d79654554e5f3cd421f9cc
Julian Clement <pieti30tv@gmail.com>
2026-04-18 20:28:59 +0200

    feat: create v1 of login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,25 @@
+import "../index.css";
+
 function Login() {
+  function login() {}
   return (
     <div>
-      <h1>Login</h1>
+      <form className="form">
+        <h1>Login</h1>
+        <input type="text" className="inp-username" placeholder="Username" />
+        <br />
+        <input
+          type="password"
+          className="inp-password"
+          placeholder="Password"
+        />
+        <br />
+        <button className="btn-login-submit btn-primary" onClick={login}>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
+
 export default Login;
